9371a167823e3fa6c8069921481c397f66c7dca5
MariaVasileva04 <masha0042912@gmail.com>
2024-09-26 14:41:00 +0300

    проверка

---
+++ Vasileva_lab1/Vasileva_1.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x;
+//	cout << "Menu:\n";
+//	cout << "1 - Add a pipe\n";
+//	cout << "2 - Add a CS\n";
+//	cout << "3 - Viewing all objects\n";
+//	cout << "4 - Edit the pipe\n";
+//	cout << "5 - Edit the CS\n";
+//	cout << "6 - Save to file\n";
+//	cout << "7 - Extract from a file\n";
+//	cout << "8 - Exit\n";
+//	cout << "Enter your choice ";
+//	cin >> x;
+//
+//	return 0;
+//
+//}
+//
+//struct Pipe {
+//	string name;
+//	int lenght;
+//	int diameter;
+//	bool remont;
+//}
+//
+//
+//struct CS {
+//	string name;
+//	int ws;
+//	int ws_inwork;
+//	char effect;
+//}
